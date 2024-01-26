00e910f11819ca61e228d3738afe042e7ebbf191
tonyandy5630 <tonyandy456@gmail.com>
2024-01-26 16:41:55 +0700

    [Tu] Add extend to Repositories

--- Repository/Repo/AddressRepo.cs
+++ Repository/Repo/AddressRepo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccessLayer.DBContext;
+using Repository.Interface;
+using Repository.Interface.IUnitOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,10 @@ using System.Threading.Tasks;
 
 namespace Repository.Repo
 {
-    public class AddressRepo
+    public class AddressRepo:IAddressRepo
     {
+        public AddressRepo(lumosdbContext context)
+        {
+        }
     }
 }
--- Repository/Repo/ServiceBookingRepo.cs
+++ Repository/Repo/ServiceBookingRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Repo
 {
-    public class ServiceBookingRepo
+    public class ServiceBookingRepo:IServiceBookingRepo
     {
     }
 }
