d65f0f62930abccc02a78e20a00c6c95ea350f1b
tonyandy5630 <tonyandy456@gmail.com>
2024-01-26 23:01:35 +0700

    index on main: f1ddd9c [Tu] Remove Favorite Partner Repository and Services

---
+++ Repository/Interface/IGenericRepository/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface.IGenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(object id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task<T> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> RemoveAsync(T entity);
+    }
+}
