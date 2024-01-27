37059edf3bde7d608663aaead8775acbe1970b20
tonyandy5630 <tonyandy456@gmail.com>
2024-01-28 01:09:26 +0700

    [Tu] Add service injection to DI

---
+++ Service/DI/DependencInjection.cs
@@ -0,0 +1,84 @@
+﻿using BussinessObject;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+using Repository.Interface;
+using Repository.Interface.IUnitOfWork;
+using Repository.Repo;
+using Service.InterfaceService;
+using Service.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DI
+{
+    public static class DependencInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration, string connectionString)
+        {
+            services.AddHttpContextAccessor();
+            services.AddDbContext<LumosDBContext>(option =>
+            option.UseSqlServer(connectionString));
+
+            #region entity
+            //unitofwork
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            //add repo
+
+            // Admin
+            services.AddTransient<IAdminRepo, AdminRepo>();
+            services.AddTransient<IAdminService, AdminService>();
+
+            // Address
+            services.AddTransient<IAddressRepo, AddressRepo>();
+            services.AddTransient<IAddressService, AddressService>();
+
+            //BookingLog
+            services.AddTransient<IBookingLogRepo, BookingLogRepo>();
+            services.AddTransient<IBookingLogService, BookingLogService>();
+
+            services.AddTransient<IBookingRepo, BookingRepo>();
+            services.AddTransient<IBookingService, BookingService>();
+
+            services.AddTransient<ICustomerRepo, CustomerRepo>();
+            services.AddTransient<ICustomerService, CustomerService>();
+
+            services.AddTransient<IHistoryLogRepo, HistoryLogRepo>();
+            services.AddTransient<IHistoryLogService, HistoryLogService>();
+
+            services.AddTransient<IMedicalReportRepo, MedicalReportRepo>();
+            services.AddTransient<IMedicalReportService, MedicalReportService>();
+
+            // Partner
+            services.AddTransient<IPartnerRepo, PartnerRepo>();
+            services.AddTransient<IPartnerService, PartnerService>();
+
+            services.AddTransient<IPartnerTypeRepo, PartnerTypeRepo>();
+            services.AddTransient<IPartnerService, PartnerService>();
+
+            services.AddTransient<IPaymentMethodRepo, PaymentMethodRepo>();
+            services.AddTransient<IPaymentMethodService, PaymentMethodService>();
+
+            services.AddTransient<IScheduleRepo, ScheduleRepo>();
+            services.AddTransient<IScheduleService, ScheduleService>();
+
+            services.AddTransient<IServiceBookingRepo, ServiceBookingRepo>();
+            services.AddTransient<IServiceBookingSer, ServiceBookingSer>();
+
+            services.AddTransient<IServiceCategoryRepo, ServiceCategoryRepo>();
+            services.AddTransient<IServiceCategorySer, ServiceCategorySer>();
+
+            services.AddTransient<ISystemConfigurationRepo, SystemConfigurationRepo>();
+            services.AddTransient<ISystemConfigurationService, SystemConfigurationService>();
+
+            #endregion
+
+
+            return services;
+        }
+    }
+}
