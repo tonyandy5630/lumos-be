dd34ef679d98584e89b0045308858f617ea234ab
nvtiendev <nvtiendev@gmail.com>
2024-01-29 15:12:26 +0700

    [VanTien][add IAuthentication and Authentication]

---
+++ Service/InterfaceService/IAuthentication .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.InterfaceService
+{
+    public interface IAuthentication
+    {
+        Task<(bool, string)> IsUserAuthenticatedAsync(string email, string password);
+        Task<(string, DateTime, string)> GenerateToken(string email, string role);
+        string GenerateRefreshToken();
+        Task SaveRefreshTokenToDatabase(string email, string refreshToken);
+        Task<(bool, string)> ValidateRefreshToken(string refreshToken);
+        Task<(bool, string)> CheckRole(string email);
+    }
+}
