488ae397877fdd3505e77ac7987b9361897da763
Đức Bùi <bhduc264@gmail.com>
2024-01-31 19:44:33 +0700

    [3mallrice][create customerController]

---
+++ LumosSolution/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.InterfaceService;
+
+namespace LumosSolution.Controllers
+{
+    [Route("api/customer")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+    }
+}
