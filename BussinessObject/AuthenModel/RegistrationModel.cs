6fcdb50ab430a2ce4df5f1ef0c1129625540feab
nvtiendev <nvtiendev@gmail.com>
2024-01-31 16:49:29 +0700

    [VanTien][Add register api]

--- BussinessObject/AuthenModel/RegistrationModel.cs
+++ BussinessObject/AuthenModel/RegistrationModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace BussinessObject.AuthenModel
 {
     public class RegistrationModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
