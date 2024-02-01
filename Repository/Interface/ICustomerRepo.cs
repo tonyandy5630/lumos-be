36cb46538d20a2789328ac9476312cff2f5fba27
Maotou <93722462+maotou-spy@users.noreply.github.com>
2024-02-01 18:03:22 +0700

    [maotou][update function] (#13)
    * [maotou][update source]
    
    * [maotou][GetCustomerAddressByCustomerIdAsync]
    
    * [maotou][update function get customer address]

--- Repository/Interface/ICustomerRepo.cs
+++ Repository/Interface/ICustomerRepo.cs
@@ -19,7 +19,6 @@ namespace Repository.Interface
         Task<bool> BanCustomerAsync(int id);
 
         Task<List<MedicalReport>> GetMedicalReportByCustomerIdAsync(int id);
-        Task<List<Address>> GetCustomersAddressByCustomerIdAsync(int id);
-
+        Task<List<Address>> GetCustomerAddressByCustomerIdAsync(int id);
     }
 }
