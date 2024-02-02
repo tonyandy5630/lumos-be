616d01e5b687c4ef236d20b9da19acfac487e002
Đức Bùi <bhduc264@gmail.com>
2024-02-02 16:58:58 +0700

    [3mallrice][GetScheduleByPartner]

--- DataAccessLayer/ScheduleDAO.cs
+++ DataAccessLayer/ScheduleDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BussinessObject;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,39 @@ namespace DataAccessLayer
 {
     public class ScheduleDAO
     {
+        private static ScheduleDAO instance = null;
+        private LumosDBContext _context = null;
+
+        public ScheduleDAO()
+        {
+            if (_context == null)
+                _context = new LumosDBContext();
+        }
+
+        public static ScheduleDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ScheduleDAO();
+                }
+                return instance;
+            }
+        }
+
+        public async Task<List<Schedule>> GetScheduleByPartnerIdAsyn(int id)
+        {
+            try
+            {
+                var schedule = await _context.Schedules.Where(u => u.PartnerId == id).ToListAsync();
+                return schedule;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in GetScheduleByPartnerIdAsyn: {ex.Message}", ex);
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
--- LumosSolution/Controllers/PartnerController.cs
+++ LumosSolution/Controllers/PartnerController.cs
@@ -102,5 +102,35 @@ namespace LumosSolution.Controllers
             }
         }
 
+        [HttpGet("{id}/schedule")]
+        [Authorize(Roles = "Partner,Customer")]
+        public async Task<ActionResult<List<Schedule>>> GetScheduleByPartnerId(int id)
+        {
+            ApiResponse<List<Schedule>> res = new ApiResponse<List<Schedule>>();
+            try
+            {
+                res.data = await _partnerService.GetScheduleByPartnerIdAsyn(id);
+                if (res.data == null || res.data.Count == 0)
+                {
+                    res.message = MessagesResponse.Error.NotFound;
+                    res.StatusCode = ApiStatusCode.NotFound;
+                }
+                else
+                {
+                    {
+                        res.message = MessagesResponse.Success.Completed;
+                        res.StatusCode = ApiStatusCode.OK;
+                    }
+                }
+
+                return Ok(res);
+            }
+            catch
+            {
+                res.message = MessagesResponse.Error.OperationFailed;
+                res.StatusCode = ApiStatusCode.BadRequest;
+                return BadRequest(res);
+            }
+        }
     }
 }
