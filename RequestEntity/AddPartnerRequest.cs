049657922d9a2cbaafb81b530a2c64452b33d41a
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-27 17:05:15 +0700

    Add partner request for creating partner

---
+++ RequestEntity/AddPartnerRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestEntity
+{
+    public class AddPartnerRequest
+    {
+        [Required]
+        public string PartnerName { get; set; }
+        public string? DisplayName { get; set; }
+        [Required]
+        public int TypeId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string? Phone { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public string? Description { get; set; }
+        public string? BusinessLicenseNumber { get; set; }
+    }
+}
