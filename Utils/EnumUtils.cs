7b09909e30b5bf7ba1d60aab79c859566ae4cad6
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-03-01 04:07:41 +0700

    [Tu] <new> Add Enum Utils

---
+++ Utils/EnumUtils.cs
@@ -0,0 +1,25 @@
+﻿using Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class EnumUtils
+    {
+        public static string GetBookingEnumByStatus(int? status)
+        {
+            return status switch
+            {
+                0 => BookingStatusEnum.Canceled.ToString(),
+                1 => BookingStatusEnum.Pending.ToString(),
+                2 => BookingStatusEnum.Doing.ToString(),
+                3 => BookingStatusEnum.Finished.ToString(),
+                4 => BookingStatusEnum.Completed.ToString(),
+                _ => "Unknown",
+            };
+        }
+    }
+}
