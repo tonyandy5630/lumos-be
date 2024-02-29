ba397f57566d8f764cf9ba66c9f0577dd78520ed
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-03-01 03:15:24 +0700

    Add Booking Status Enum

---
+++ Enums/BookingStatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public enum BookingStatusEnum
+    {
+        Canceled,
+        Pending,
+        Doing ,
+        Finished,
+        Completed
+    }
+}
