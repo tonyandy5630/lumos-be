e313ad985de019c72150b4b06b622166d4fff98b
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-03-01 19:49:25 +0700

    [Tu] Redundant utils

--- Utils/StringUtil.cs
+++ Utils/StringUtil.cs
@@ -10,10 +10,6 @@ namespace Utils
 {
     public static class StringUtil
     {
-        public static string Hash(this string input) => BCrypt.Net.BCrypt.HashPassword(input);
-
-        public static bool CheckPassword(this string password, string hashPassword) => BCrypt.Net.BCrypt.Verify(password, hashPassword);
-
         private static Random random = new Random();
 
         public static string RandomString(int length)
