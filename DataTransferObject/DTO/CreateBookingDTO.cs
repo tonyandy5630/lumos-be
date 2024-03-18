5ec41a2ef84df0f7d28d26d5b47a4efc52f34fbd
nvtiendev <nvtiendev@gmail.com>
2024-03-18 23:35:39 +0700

    [VanTien][Check validate for CreateBooking]

--- DataTransferObject/DTO/CreateBookingDTO.cs
+++ DataTransferObject/DTO/CreateBookingDTO.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Utils.Validation;
 
 namespace DataTransferObject.DTO
 {
@@ -14,10 +16,15 @@ namespace DataTransferObject.DTO
         [JsonIgnore]
         public int TotalPrice { get; set; }
         public DateTime BookingDate { get; set; }
-/*        public DateTime From { get; set; }*/
+        /*        public DateTime From { get; set; }*/
+        [Required(ErrorMessage = "DayOfWeek is required")]
+        [Range(2, 8, ErrorMessage = "DayOfWeek must be between 2 and 8")]
         public int DayOfWeek { get; set; }
+        [Required(ErrorMessage = "WorkShift is required")]
+        [Range(1, 3, ErrorMessage = "WorkShift must be between 1 and 3")]
         public int bookingTime { get; set; }
         public string Address { get; set; }
+        [BadWord(ErrorMessage = "Note contains a forbidden word.")]
         public string Note { get; set; }
         [JsonIgnore]
         public string? PaymentLinkId { get; set; }
